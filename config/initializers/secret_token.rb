3076ec43f961c817d893b338fca694e6efdbde29
Shree Durga <shree.durga@gmail.com>
2012-02-09 13:00:12 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScheduleTron2000::Application.config.secret_token = 'd7ca8b03cf91c77cd33dcad3fa907962a920d6bc19427f62d9b3a3166fad12dab1714feb9c91137640af85ccd9546db10b597b92e9a5a4e606ae422dfc8187f0'
