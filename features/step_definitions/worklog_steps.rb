3d5fbb9283af5a62847460c7f737ebc3806ab52d
“Shree <shree.durga@gmail.com>
2012-03-21 12:01:03 -0500

    checks if user_id for worklogs are saved

--- features/step_definitions/worklog_steps.rb
+++ features/step_definitions/worklog_steps.rb
@@ -1,6 +1,5 @@
 Given /^That I am logged in$/ do
   @user = Factory :user
- 
   visit new_user_session_path
   fill_in 'Email', with: @user.email
   fill_in 'Password', with: @user.password
@@ -23,10 +22,18 @@ Then /^I should be able to create and save new worklogs$/ do
   current_path.should == worklog_path(Worklog.last.id)
 end
 
-Then /^The last worklog listed should be by me$/ do
-  w = Worklog.last
-  w.time_period.should ==Date.parse('Mon, 19 Mar 2012')
-  w.user_id.should == @user.id
+Given /^I create a new worklog$/ do
+  visit new_worklog_path
+  fill_in 'Who', with: 'Name'
+  fill_in 'Work progress this week', with: 'work this week'
+  fill_in 'Concerns', with: 'concerns'
+  select('Week of 2012-03-19',from:'Time period')
+  click_button 'Create Worklog'
+  Worklog.where(:user_id => @user.id).last.should_not be nil
+end
+
+Then /^The last worklog in the database should be saved by me$/ do
+
 end
 
 
--- features/worklog.feature
+++ features/worklog.feature
@@ -4,6 +4,9 @@ Feature: Worklog features
     Given That I am logged in 
     And I visit the worklogs page
     Then I should be able to create and save new worklogs
-    And The last worklog listed should be by me
   
+  Scenario: Worklogs cannot be saved without an associated User
+    Given That I am logged in
+    And I create a new worklog
+    Then The last worklog in the database should be saved by me
  
